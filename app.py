dbca7c7fced3b9549a6826a1b4d1e43de1bfc067
MR.Parth mahadik <parthmahadik44@gmail.com>
2026-01-11 20:52:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ def load_trained_model():
     global model
     try:
         # Use the same model file you used with FastAPI
-        model_path = os.path.join(os.path.dirname(__file__), 'model', 'model_with_inference_2.h5')
-        model = tf.keras.models.load_model(model_path)
+        model = tf.keras.models.load_model("model_with_inference_2.h5")
 
     except Exception as e:
         print(f"❌ Error loading model: {e}")
@@ -148,5 +147,6 @@ def predict():
 if __name__ == '__main__':
     load_trained_model()
     app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 
